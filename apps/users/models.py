188d6c407881222284d036ee8cb7f18b16e8a9ae
KAITO-VL <adisonvictor320@gmail.com>
2026-01-16 18:26:50 -0500

    Creacion del modelo customer en la app de users

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=70, blank=True)
+    phone = models.CharField(max_length=20, blank=True)
