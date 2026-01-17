ed6407fb2559a3a6c9d9cba923faf9dc755cb4f1
KAITO-VL <adisonvictor320@gmail.com>
2026-01-17 00:18:21 -0500

    Eliminacion de las notas de lo modelos, tambien se renombro el atributo de barcoder a barcode(este es el mismo que estaba en la documentacion del domain)

--- apps/inventory/models.py
+++ apps/inventory/models.py
@@ -7,12 +7,8 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=70)
-    barcoder = models.CharField(max_length=70,unique=True)
+    barcode = models.CharField(max_length=70,unique=True)
     category = models.ForeignKey(Category,on_delete=models.PROTECT)
     price = models.DecimalField(max_digits=8,decimal_places=2)
     stock_quantity = models.PositiveSmallIntegerField()
     is_active = models.BooleanField(default=False)
-
-    """inventario: product y category
-ventas: sale y saleitem
-usuarios: customer and user(staff)"""
