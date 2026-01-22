5a6960513819f3bbc84749a26b10eb01ed7698c2
KAITO-VL <adisonvictor320@gmail.com>
2026-01-21 23:29:37 -0500

    Añadido de estandar de datos en datos del cliente(name y phone), con la finalidad de evitar campos vacios

--- apps/users/models.py
+++ apps/users/models.py
@@ -4,3 +4,16 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=70, blank=True)
     phone = models.CharField(max_length=20, blank=True)
+
+    def save(self,*args,**kwargs):
+        if self.name:
+            self.name = self.name.strip()
+        if not self.name:
+            self.name = 'ANONIMO'
+
+        if self.phone:
+            self.phone = self.phone.strip()
+        if not self.phone:
+            self.phone = '000000000'
+        self.full_clean()
+        super().save(*args,**kwargs)
