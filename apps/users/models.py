57bbce8cc9ed5b97b9e81474f33daa19328c80f5
KAITO-VL <adisonvictor320@gmail.com>
2026-01-21 23:41:20 -0500

    Añadido de metodo str al modelo de Customer

--- apps/users/models.py
+++ apps/users/models.py
@@ -17,3 +17,6 @@ class Customer(models.Model):
             self.phone = '000000000'
         self.full_clean()
         super().save(*args,**kwargs)
+    
+    def __str__(self):
+        return self.name
