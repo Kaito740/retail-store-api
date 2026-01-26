071bd9edbca2df350f40404f40f25d194b2c6790
KAITO-VL <adisonvictor320@gmail.com>
2026-01-25 22:55:32 -0500

    Creacion de services del app sales

---
+++ apps/sales/services.py
@@ -0,0 +1,46 @@
+from django.db import transaction
+from apps.inventory.models import Product
+from apps.users.models import Customer
+from .models import Sale, SaleItem
+from django.core.exceptions import ValidationError
+from decimal import Decimal
+
+def _verify_products(product_id, quantity_requested):
+    product_data = Product.objects.filter(id=product_id).first()
+    if not product_data:
+        raise ValidationError('El producto no fue encontrado.')
+    if product_data.stock_quantity < quantity_requested:
+        raise ValidationError(f'Stock insuficiente para: {product_data.name}. '
+                              f'Disponible: {product_data.stock_quantity}')
+    return product_data
+
+def _calculate_subtotal(quantity, unit_price):
+    subTotal = Decimal(quantity)  * Decimal(unit_price)
+    return subTotal
+
+def sale_create(*, created_by, products_data, customer=None):
+    with transaction.atomic():
+        sale = Sale.objects.create(
+            created_by=created_by,
+            customer=customer,
+            status=Sale.Status.DRAFT
+        )
+        accumulated_total = Decimal('0.00')
+
+        for item in products_data:
+            product = _verify_products(item['product_id'], item['quantity'])
+            current_subtotal = _calculate_subtotal(item['quantity'], product.price)
+            
+            SaleItem.objects.create(
+                sale=sale,
+                product=product,
+                quantity=item['quantity'],
+                unit_price=product.price,
+            )
+
+            accumulated_total += current_subtotal
+
+        sale.total_amount = accumulated_total
+        sale.save() 
+
+    return sale
