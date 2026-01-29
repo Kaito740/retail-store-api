a3e27cd195a9375e71d161b132889b07a0106aa6
KAITO-VL <adisonvictor320@gmail.com>
2026-01-28 23:55:06 -0500

    Definir view de Category(GET, POST) y view de Category(PUT, DELETE)

--- apps/inventory/views.py
+++ apps/inventory/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Category
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .serializers import CategorySerializer
 # Create your views here.
+
+class CategoryListCreateView(ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class CategoryRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
