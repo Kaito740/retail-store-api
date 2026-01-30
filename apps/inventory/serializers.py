d68440644687769ac2675f43215bfbb2f696aebb
KAITO-VL <adisonvictor320@gmail.com>
2026-01-29 23:03:13 -0500

    Se definio el serializador del modelo Product

--- apps/inventory/serializers.py
+++ apps/inventory/serializers.py
@@ -1,8 +1,15 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Category
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
+from .models import Category, Product
 
 class CategorySerializer(ModelSerializer):
     class Meta:
         model = Category
         fields = ['id','name','is_active']
+        read_only_fields = ['id']
+
+class ProductSerializer(ModelSerializer):
+    category = PrimaryKeyRelatedField(queryset=Category.objects.all())
+    class Meta:
+        model = Product
+        fields = ['id','name','barcode','category','price','stock_quantity','is_active']
         read_only_fields = ['id']
