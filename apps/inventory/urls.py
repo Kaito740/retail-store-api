b17a1cfebe7933c47b8ab6eae12304892409d328
KAITO-VL <adisonvictor320@gmail.com>
2026-01-29 23:05:05 -0500

    Se definio los endpoints de products

--- apps/inventory/urls.py
+++ apps/inventory/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import CategoryListCreateView, CategoryRetrieveUpdateDestroyView
+from .views import CategoryListCreateView, CategoryRetrieveUpdateDestroyView, ProductListCreateView, ProductRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('categories/', CategoryListCreateView.as_view(), name='category-list'),
-    path('categories/<int:pk>/', CategoryRetrieveUpdateDestroyView.as_view(), name='category-rud')
+    path('categories/<int:pk>/', CategoryRetrieveUpdateDestroyView.as_view(), name='category-rud'),
+    path('products/',ProductListCreateView.as_view(),name='product-list'),
+    path('products/<int:pk>/', ProductRetrieveUpdateDestroyView.as_view(),name='product-rud')
 ]
