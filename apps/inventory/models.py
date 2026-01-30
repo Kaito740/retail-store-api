2dc3e5a91836d43c4a9743f82419dec61d21ac37
KAITO-VL <adisonvictor320@gmail.com>
2026-01-29 23:02:28 -0500

    Se modifico el parametro MinValueValidator del atributo price del modelo de Product, se importo y se uso Decimal apra este parametro

--- apps/inventory/models.py
+++ apps/inventory/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from decimal import Decimal
 from django.core.validators import MinLengthValidator, MinValueValidator
 
 # Create your models here.
@@ -16,7 +17,7 @@ class Product(models.Model):
     name = models.CharField(max_length=70, validators=[MinLengthValidator(2,message='El nombre del producto es muy corto.')])
     barcode = models.CharField(max_length=70,unique=True)
     category = models.ForeignKey(Category,on_delete=models.PROTECT)
-    price = models.DecimalField(max_digits=8,decimal_places=2,validators=[MinValueValidator(0.00,message='El precio no puede ser negativo.')])
+    price = models.DecimalField(max_digits=8,decimal_places=2,validators=[MinValueValidator(Decimal('0.00'),message='El precio no puede ser negativo.')])
     stock_quantity = models.PositiveSmallIntegerField()
     is_active = models.BooleanField(default=False)
     
