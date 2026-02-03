c443aeb838482210aec0634655f4d9ccf121ff5c
KAITO-VL <adisonvictor320@gmail.com>
2026-02-02 21:14:18 -0500

    Se definio las views CRUD del modelo Customer del app users

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .serializers import CustomerSerializer
+from .models import Customer
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 # Create your views here.
+class ListCreateCustomerView(ListCreateAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+class RetrieveUpdateDestroyCustomerView(RetrieveUpdateDestroyAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
