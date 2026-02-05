f24338c06072430e0f75cdf18d6529d5d26224c8
KAITO-VL <adisonvictor320@gmail.com>
2026-02-04 23:33:45 -0500

    Definicion de rutas del modelo de customer y user del app users

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import ListCreateCustomerView, RetrieveUpdateDestroyCustomerView
+from .views import CustomerListCreateView, CustomerDetailView, UserRegisterView, UserListView, UserDetailView
 
 urlpatterns = [
-    path('customers/',ListCreateCustomerView.as_view(),name='customer-list'),
-    path('customers/<int:pk>/', RetrieveUpdateDestroyCustomerView.as_view(),name='customer-rud')
+    path('customers/', CustomerListCreateView.as_view()),
+    path('customers/<int:pk>/', CustomerDetailView.as_view()),
+    path('register/', UserRegisterView.as_view()),
+    path('', UserListView.as_view()),
+    path('<int:pk>/', UserDetailView.as_view()),
 ]
