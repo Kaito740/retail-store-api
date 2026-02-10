61841d9d6d5693753a45b859332eaba38d0bf676
KAITO-VL <adisonvictor320@gmail.com>
2026-02-09 22:20:11 -0500

    Definicion de display administrativo de sales

--- apps/sales/admin.py
+++ apps/sales/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
-
+from .models import Sale, SaleItem
 # Register your models here.
+class SaleItemInline(admin.TabularInline):
+    model = SaleItem
+    extra = 0
+    min_num = 1
+    readonly_fields = ['product','quantity','unit_price','subtotal']
+
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ['id','customer', 'total_amount', 'created_by',  'status', 'created_at']
+    inlines = [SaleItemInline]
+    search_fields = ['id','name','phone']
+    list_filter = ['created_by','status']
+    readonly_fields = ['customer','total_amount','created_by','status','created_at']
+
+    def has_add_permission(self, request):
+        return False
+    
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
